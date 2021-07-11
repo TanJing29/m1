4dfede94e1e76986cec5d2132e54b8823c579fef
huduanduan <3215058177@qq.com>
2021-07-11 21:58:22 +0800

    胡诗情第一次提交

---
+++ medical_care_after/src/main/java/com/trkj/medical_care_after/entity/Inventory.java
@@ -0,0 +1,78 @@
+package com.trkj.medical_care_after.entity;
+
+import java.io.Serializable;
+import lombok.Data;
+
+/**
+ * inventory
+ * @author 
+ */
+@Data
+public class Inventory implements Serializable {
+    /**
+     * 库存信息id
+     */
+    private Integer inId;
+
+    /**
+     * 供货商名称
+     */
+    private String inSupplier;
+
+    /**
+     * 库存上限
+     */
+    private Integer inUpperlimit;
+
+    /**
+     * 库存下限
+     */
+    private Integer inLowerlimit;
+
+    /**
+     * 备注
+     */
+    private String inDescribe;
+
+    /**
+     * 原始数量
+     */
+    private Integer inOriginal;
+
+    /**
+     * 出库数量
+     */
+    private Integer inStock;
+
+    /**
+     * 报损数量
+     */
+    private Integer inReportedloss;
+
+    /**
+     * 销售数量
+     */
+    private Integer inSales;
+
+    /**
+     * 入库退货
+     */
+    private Integer inReceiptReturn;
+
+    /**
+     * 入库数量
+     */
+    private Integer inQuantity;
+
+    /**
+     * 状态（超储/正常/缺货）
+     */
+    private String inState;
+
+    /**
+     * 商品id
+     */
+    private Integer inProid;
+
+    private static final long serialVersionUID = 1L;
+}
